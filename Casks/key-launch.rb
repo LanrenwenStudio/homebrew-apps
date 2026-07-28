f0350f2969a4db36c9197be9870429c31b77abc3
烂人文 <kevinxft@gmail.com>
2026-07-28 13:48:11 +0800

    Update KeyLaunch to 1.3.8

--- Casks/key-launch.rb
+++ Casks/key-launch.rb
@@ -1,8 +1,8 @@
 cask "key-launch" do
-  version "1.3.7"
-  sha256 "065283b505ed5d44ac2d17b9fea00eb47aef0c37f476e0a45aec047783d7056d"
+  version "1.3.8"
+  sha256 "ee560d1366f61d9f2e0a9ba5ffb86433796f472bf61fbc82f2bd88bdee88023c"
 
-  url "https://github.com/LanrenwenStudio/homebrew-apps/releases/download/key-launch-v#{version}/key-launch-#{version}.dmg"
+  url "https://github.com/LanrenwenStudio/homebrew-apps/releases/download/key-launch-v\#{version}/key-launch-\#{version}.dmg"
   name "KeyLaunch"
   desc "Launch apps with global keyboard shortcuts"
   homepage "https://lanrenwenstudio.github.io/keylaunch-site/"
