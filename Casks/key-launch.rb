8dd5061d5c7948e4dba49f2f0e0e4a85817a0778
烂人文 <kevinxft@gmail.com>
2026-07-29 00:58:27 +0800

    chore(cask): update key-launch to 1.3.9

--- Casks/key-launch.rb
+++ Casks/key-launch.rb
@@ -1,6 +1,6 @@
 cask "key-launch" do
-  version "1.3.8"
-  sha256 "ee560d1366f61d9f2e0a9ba5ffb86433796f472bf61fbc82f2bd88bdee88023c"
+  version "1.3.9"
+  sha256 "3201e4234f0321c1cdb4c9f904d2ed6a67b62c8ab4df96566634fa19ef959016"
 
   url "https://github.com/LanrenwenStudio/homebrew-apps/releases/download/key-launch-v#{version}/key-launch-#{version}.dmg"
   name "KeyLaunch"
