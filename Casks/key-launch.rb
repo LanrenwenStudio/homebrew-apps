04c568228d5ecadecf51009b41d4301fea7947d7
烂人文 <kevinxft@gmail.com>
2026-07-28 14:03:50 +0800

    fix(cask): fix Ruby string interpolation in key-launch.rb URL stanza

--- Casks/key-launch.rb
+++ Casks/key-launch.rb
@@ -2,7 +2,7 @@ cask "key-launch" do
   version "1.3.8"
   sha256 "ee560d1366f61d9f2e0a9ba5ffb86433796f472bf61fbc82f2bd88bdee88023c"
 
-  url "https://github.com/LanrenwenStudio/homebrew-apps/releases/download/key-launch-v\#{version}/key-launch-\#{version}.dmg"
+  url "https://github.com/LanrenwenStudio/homebrew-apps/releases/download/key-launch-v#{version}/key-launch-#{version}.dmg"
   name "KeyLaunch"
   desc "Launch apps with global keyboard shortcuts"
   homepage "https://lanrenwenstudio.github.io/keylaunch-site/"
