452343b7e4317ccb9ed46333b2cfcf236e99490c
烂人文 <kevinxft@gmail.com>
2026-08-21 15:57:01 +0800

    release: deploy KeyLaunch website 1.3.19

--- Casks/key-launch.rb
+++ Casks/key-launch.rb
@@ -5,7 +5,7 @@ cask "key-launch" do
   url "https://github.com/LanrenwenStudio/homebrew-apps/releases/download/key-launch-v#{version}/key-launch-#{version}.dmg"
   name "KeyLaunch"
   desc "Launch apps with global keyboard shortcuts"
-  homepage "https://lanrenwenstudio.github.io/keylaunch-site/"
+  homepage "https://keylaunch.lanrenwen.com/"
 
   depends_on :macos
 
