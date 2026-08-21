230a645d52a04311bd47e71935579d656841d17b
烂人文 <kevinxft@gmail.com>
2026-08-21 16:56:07 +0800

    Update KeyLaunch to 1.3.20

--- Casks/key-launch.rb
+++ Casks/key-launch.rb
@@ -1,6 +1,6 @@
 cask "key-launch" do
-  version "1.3.19"
-  sha256 "bbeaeae0d0b5c2a15ebd848323d5433eab84669953cca3c952d302056424d22d"
+  version "1.3.20"
+  sha256 "864b717cb21e120c2b0f1ef8350c5d22b611248f462b8bc6de5422743ed68133"
 
   url "https://github.com/LanrenwenStudio/homebrew-apps/releases/download/key-launch-v#{version}/key-launch-#{version}.dmg"
   name "KeyLaunch"
