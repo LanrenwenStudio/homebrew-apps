46ad2e37dd30b74d8e9c3f964d4570770747de71
烂人文 <kevinxft@gmail.com>
2026-08-22 19:20:33 +0800

    chore: update PauseLoop 1.0.2 cask

--- Casks/pause-loop.rb
+++ Casks/pause-loop.rb
@@ -1,6 +1,6 @@
 cask "pause-loop" do
-  version "1.0.1"
-  sha256 "6ef30f09e575b820524b75ab3438622501dc33cfea649be69fc4ce2d6ee3e03c"
+  version "1.0.2"
+  sha256 "ae83fd0fbc66902d33b7fe829ae6b0969311eb4be99f76ae79566829f770166c"
 
   url "https://github.com/LanrenwenStudio/homebrew-apps/releases/download/pause-loop-v#{version}/pause-loop-#{version}.dmg"
   name "PauseLoop"
